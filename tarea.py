06706a1b65b491205834db5ddf6128838e0bc52f
MariaCuenca26 <86026443+MariaCuenca26@users.noreply.github.com>
2021-07-17 22:52:11 -0500

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,9 @@
+class tarea:
+    def __init__(self):
+        pass
+    def calculoPago(self):
+        ht, he, het=0,0,0
+        ph, phe, pt =0,0,0
+        ht = int(input("Ingrese horas trabajadas:"))
+        ph = float(input("ingrese valor hota:"))
+        
